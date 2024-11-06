fd9dcb21bc40fc19be44a1c046b0be38dea57e00
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-11-07 00:23:32 +0100

    Feat: Create Navigation menu for smartphones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react({experimentalReactChildren: true}), tailwind()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+  }
+});
