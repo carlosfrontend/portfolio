ccc3a4df0c67563a5f7ab385c668d25319215ba6
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-03-31 01:04:48 +0200

    Fix: Refactor .gitignore

--- .netlify/v1/functions/ssr/.netlify/build/chunks/home-page_BEWsAeiD.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./home-page_8kOuImDn.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- .netlify/v1/functions/ssr/.netlify/build/chunks/weather-app_JOJikTdV.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./weather-app_KWK1j4_G.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
